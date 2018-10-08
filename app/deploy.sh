98f02965a529f2f58d452157263b888e7f2bc3a8
benjithedalilama <benjithedalillama@gmail.com>
2018-10-08 10:36:15 +0100

    modified deployment script

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,5 +1,5 @@
 IMAGE="benjithedalilama/alice-backend:MVP"
 
-./hyper pull $IMAGE
-./hyper compose down -p backend
-./hyper compose up -p backend -f hyper-compose.yml -d
+../hyper pull $IMAGE
+../hyper compose down -p backend
+../hyper compose up -p backend -f hyper-compose.yml -d
