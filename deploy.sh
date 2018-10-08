d5b55210d7592f4bfd101eb2c1601733b5dbe158
benjithedalilama <benjithedalillama@gmail.com>
2018-10-08 10:17:32 +0100

    trimmed down and broke up deployment and image building/pushing scripts, added .nvmrc

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 IMAGE="benjithedalilama/alice-backend:MVP"
 
-docker build . -t $IMAGE
-docker login
-docker push $IMAGE
-
-hyper pull $IMAGE
-
-hyper compose down -p backend
-hyper compose up -p backend -f hyper-compose.yml
+./hyper pull $IMAGE
+./hyper compose down -p frontend
+./hyper compose up -p frontend -f hyper-compose.yml -d
