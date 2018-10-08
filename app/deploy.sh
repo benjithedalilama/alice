42427f37528f84790e305b8ee6442343b62bc30d
benjithedalilama <benjithedalillama@gmail.com>
2018-10-08 10:32:54 +0100

    changed hyper path

---
+++ app/deploy.sh
@@ -0,0 +1,5 @@
+IMAGE="benjithedalilama/alice-backend:MVP"
+
+~/hyper pull $IMAGE
+~/hyper compose down -p backend
+~/hyper compose up -p backend -f hyper-compose.yml -d
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-IMAGE="benjithedalilama/alice-backend:MVP"
-
-./hyper pull $IMAGE
-./hyper compose down -p backend
-./hyper compose up -p backend -f hyper-compose.yml -d
