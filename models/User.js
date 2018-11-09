9cc6b391c625043280119af76ed40058523fa48d
benjithedalilama <benjithedalillama@gmail.com>
2018-11-09 13:26:06 +0000

    converted some callbacks into async/await

--- models/User.js
+++ models/User.js
@@ -10,43 +10,47 @@ const UserSchema = new Schema({
     id: mongoose.Types.ObjectId,
     username: {
         type: String,
+        required: true,
         unique: true,
+        trim: true,
         index: {
             unique: true
         }
     },
     password: {
         type: String,
+        required: true,
+        trim: true,
         default: ''
     },
     email: String,
     hubs: [Hub.schema]
 })
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', async function(next) {
     const user = this
 
     if (!user.isModified('password')) return next()
 
-    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-        if (err) return next(err)
-
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if (err) return next(err)
-
-            // override the cleartext password with the hashed one
-            user.password = hash
-            next()
-        })
-    })
+    try {
+      const salt = await bcrypt.genSalt(SALT_WORK_FACTOR)
+      const hash = await bcrypt.hash(user.password, salt)
+      user.password = hash
+      next()
+    }
+    catch (err) {
+      next(err)
+    }
 })
 
-UserSchema.methods.comparePassword = (candidatePassword, cb) => {
-    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-        if (err) return cb(err)
-        cb(null, isMatch)
-    })
+UserSchema.methods.comparePassword = async function(candidatePassword, cb) {
+    try {
+      const isMatch = await bcrypt.compare(candidatePassword, this.password)
+      cb(null, isMatch)
+    }
+    catch (err) {
+      cb(err)
+    }
 }
 
 const User = mongoose.model('User', UserSchema)
